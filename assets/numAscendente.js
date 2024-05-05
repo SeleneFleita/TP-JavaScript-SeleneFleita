05dad5b4c2ed97d48952739cd0fdd240c3729cc5
Selene Fleita <selene.f353@gmailcom>
2024-05-05 17:52:28 -0300

    se trabajo con el archivo numAscendente.js y se probo en la consola

--- assets/numAscendente.js
+++ assets/numAscendente.js
@@ -1,4 +1,22 @@
 let num1 = prompt("Escriba el primer numero: ")
-let num2 = prompt("Escriba el primer numero: ")
-let num3 = prompt("Escriba el primer numero: ")
+let num2 = prompt("Escriba el segundo numero: ")
+let num3 = prompt("Escriba el tercer numero: ")
+
+if (isNaN(num1 || num2 || num3)){
+    console.log("ingrese un caracter valido")
+} else if (num1 == num2 && num2 == num3 && num3 == num1){
+    console.log("los numeros ingresados tienen el mismo valor")
+} else if ( num1 >= num2 && num1 >= num3 && num3 >= num2){
+    console.log(num2, ", ", num3,", ",num1)
+} else if ( num1 >= num2 && num1 >= num3 && num2 >= num3){
+    console.log(num3, ", ", num2, ", ", num1)
+} else if ( num2 >= num1 && num2 >= num3 && num3 >= num1){
+    console.log(num1, ", ", num3,",", num2)
+} else if ( num2 >= num1 && num2 >= num3 && num1 >= num3){
+    console.log(num3, ", ", num1, ",", num2)
+} else if ( num3 >= num1 && num3 >= num2 && num2 >= num1){
+    console.log(num1, ", ", num2, ", ", num3)
+}else if ( num3 >= num1 && num3 >= num2 && num1 >= num2){
+    console.log(num2, ", ", num1, ", ", num3)}
+
 
