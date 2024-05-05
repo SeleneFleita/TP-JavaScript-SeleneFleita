c08a2e7e3ce96fe61f6b7354ed233adb441a8edf
Selene Fleita <selene.f353@gmailcom>
2024-05-04 21:13:53 -0300

    se trabajo y termino con el archivo parimpar.js, cumple su funcion

--- assets/parImpar.js
+++ assets/parImpar.js
@@ -1,4 +1,11 @@
-let num = prompt("ingrese un numero entero")
-if (num % 2 ){
+let num =prompt("ingrese un numero entero")
 
+
+if ( isNaN(num)){
+console.log("El numero ingresado no es valido")
 }
+ else if (num % 2 === 0 ){ 
+    console.log("El numero ingresado es par")
+} else{
+    console.log("El numero ingresado es impar")
+} 
