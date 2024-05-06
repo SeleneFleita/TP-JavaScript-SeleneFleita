7236e6cc2807ff3345b79f714f9e776fc4b1200a
Selene Fleita <selene.f353@gmail.com>
2024-05-06 09:47:12 -0300

    se hizo una revision y prueba de todos los archivos

--- assets/numAscendente.js
+++ assets/numAscendente.js
@@ -1,6 +1,7 @@
-let num1 = prompt("Escriba el primer numero: ")
-let num2 = prompt("Escriba el segundo numero: ")
-let num3 = prompt("Escriba el tercer numero: ")
+let num1 = parseInt(prompt("Escriba el primer numero: "))
+let num2 = parseInt(prompt("Escriba el segundo numero: "))
+let num3 = parseInt(prompt("Escriba el tercer numero: "))
+
 
 if (isNaN( num1)){
     console.log("ingrese un caracter valido")
@@ -8,19 +9,18 @@ if (isNaN( num1)){
     console.log("ingrese un caracter valido")
 } else if (isNaN( num3)){
     console.log("ingrese un caracter valido")
-}else if (num1 == num2 && num2 == num3 && num3 == num1){
+}else if ((num1 == num2) && (num2 == num3) && (num3 == num1)){
     console.log("los numeros ingresados tienen el mismo valor")
-} else if ( num1 >= num2 && num1 >= num3 && num3 >= num2){
-    console.log(num2, ", ", num3,", ",num1)
-} else if ( num1 >= num2 && num1 >= num3 && num2 >= num3){
-    console.log(num3, ", ", num2, ", ", num1)
-} else if ( num2 >= num1 && num2 >= num3 && num3 >= num1){
-    console.log(num1, ", ", num3,",", num2)
-} else if ( num2 >= num1 && num2 >= num3 && num1 >= num3){
-    console.log(num3, ", ", num1, ",", num2)
-} else if ( num3 >= num1 && num3 >= num2 && num2 >= num1){
-    console.log(num1, ", ", num2, ", ", num3)
-}else if ( num3 >= num1 && num3 >= num2 && num1 >= num2){
-    console.log(num2, ", ", num1, ", ", num3)}
-
-
+} else if ( (num1 >= num2) && (num1 >= num3) && (num3 >= num2)){
+    console.log(num2, num3,num1)
+} else if ( (num1 >= num2) && (num1 >= num3) && (num2 >= num3)){
+    console.log(num3, num2,  num1)
+}else if ( (num2 >= num1) && (num2 >= num3) && (num3 >= num1)){
+    console.log(num1,num3,num2)
+} else if ( (num2 >= num1) && (num2 >= num3) && (num1 >= num3)){
+    console.log(num3, num1,num2)
+} else if ( (num3 >= num1) && (num3 >= num2) && (num2 >= num1)){
+    console.log(num1, num2, num3)
+}else if ( (num3 >= num1) && (num3 >= num2) && (num1 >= num2)){
+    console.log(num2, num1, num3)
+}
--- assets/parImpar.js
+++ assets/parImpar.js
@@ -3,8 +3,7 @@ let num =prompt("ingrese un numero entero")
 
 if ( isNaN(num)){
 console.log("El numero ingresado no es valido")
-}
- else if (num % 2 === 0 ){ 
+}else if (num % 2 === 0 ){ 
     console.log("El numero ingresado es par")
 } else{
     console.log("El numero ingresado es impar")
