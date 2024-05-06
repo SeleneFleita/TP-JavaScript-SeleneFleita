d3bdd1cb154e3a12f884d656e5157c3ed0677e20
Selene Fleita <selene.f353@gmail.com>
2024-05-06 09:17:09 -0300

    se uso el switch para el archivo diaSemana.js

--- assets/diaSemana.js
+++ assets/diaSemana.js
@@ -1,22 +1,48 @@
-let dia = prompt("ingrese un numero del 1 al 7: ")
+let dia = Number(prompt("ingrese un numero del 1 al 7: "))
 
-if (isNaN(dia)){
-console.log("ingrese un caracter valido")
-}else if( dia == 1){
-    console.log("El numero ingresado corresponde al dia Lunes")
-}else if( dia == 2 ){
-console.log("El numero ingresado corresponde al dia Martes")
-} else if (dia == 3){
-console.log("El numero ingresado corresponde al dia Miercoles")
-} else if (dia == 4){
-    console.log("El numero ingresado corresponde al dia Jueves")
-}else if (dia == 5){
-    console.log("El numero ingresado corresponde al dia Viernes")
-} else if (dia == 6){
-    console.log("El numero ingresado corresponde al dia Sabado")
-}else if (dia == 7){
-        console.log("El numero ingresado corresponde al dia Sabado")
-} else if ( dia >= 8){
-    console.log("ingrese un numero del 1 al 7")
+// if (isNaN(dia)){
+// console.log("ingrese un caracter valido")
+// }else if( dia == 1){
+//     console.log("El numero ingresado corresponde al dia Lunes")
+// }else if( dia == 2 ){
+// console.log("El numero ingresado corresponde al dia Martes")
+// } else if (dia == 3){
+// console.log("El numero ingresado corresponde al dia Miercoles")
+// } else if (dia == 4){
+//     console.log("El numero ingresado corresponde al dia Jueves")
+// }else if (dia == 5){
+//     console.log("El numero ingresado corresponde al dia Viernes")
+// } else if (dia == 6){
+//     console.log("El numero ingresado corresponde al dia Sabado")
+// }else if (dia == 7){
+//         console.log("El numero ingresado corresponde al dia Sabado")
+// } else if ( dia >= 8){
+//     console.log("ingrese un numero del 1 al 7")
 
-}
+// }
+switch(dia){
+    case 1:
+        console.log("el numero ingresado corresponde al dia lunes")
+    break;
+    case 2: 
+    console.log("el numero ingresado corressponde al dia martes")
+    break;
+    case 3:
+    console.log("el numero ingresado corresponde al dia miercoles")
+    break;
+    case 4:
+    console.log("el numero ingresado corresponde al dia jueves")
+    break;
+    case 5:
+    console.log("el numero ingresado corresponde al dia viernes")
+    break;
+    case 6:
+    console.log("el numero ingresado corresponde al dia sabado")
+    break;
+    case 7: 
+    console.log("el numero ingresado corresponde al dia domingo")
+    break;
+    default:
+    console.log("el caracter ingresado no es valido")
+    break;
+} 
