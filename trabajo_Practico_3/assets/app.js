cf029414ba1d70296fff9d70827504430676c22f
Selene Fleita <selene.f353@gmail.com>
2024-05-20 13:41:21 -0300

    se hizo las funciones y se trabajo en las validaciones para interaccion de usuario

--- trabajo_Practico_3/assets/app.js
+++ trabajo_Practico_3/assets/app.js
@@ -2,28 +2,48 @@ function control(nomTarea) {
     const regex = /^[a-zA-Z0-9]+$/;
     return regex.test(nomTarea);
 }
-
-
 function agregarTarea(nomTarea) {
-    tarea.push(nomTarea)
-    alert(tarea)
+    if (control(nomTarea)) {
+        if (!tarea.includes(nomTarea)) {
+            tarea.push(nomTarea)
+            alert(`Tarea agregada: ${nomTarea}`)
+        } else {
+            alert("La tarea ya esta incluida en la lista")
+        }
+    } else {
+        alert("No es valido el valor ingresado")
+    }
 }
-function listarTarea(){
-    alert(tarea)
+function listarTarea() {
+    alert(`Las tareas existentes son: ${tarea}`)
 }
-function editarTarea(nomTarea){
-    let nuevoNom = prompt("Ingrese nuevo nombre de tarea: ")
-tarea.splice(tarea.indexOf(nomTarea),1, nuevoNom)
+function editarTarea(nomTarea) {
+    if (tarea.includes(nomTarea)) {
+        let nuevoNom = prompt("Ingrese nuevo nombre de tarea: ")
+        if (control(nuevoNom)) {
+            if (!tarea.includes(nuevoNom)) {
+                tarea.splice(tarea.indexOf(nomTarea), 1, nuevoNom)
+            } else {
+                alert("La tarea ya esta incluida en la lista")
+            }
+        } else {
+            alert("No es valido el valor ingresado")
+        }
+    } else {
+        alert(" La tarea ingresada no existe en la lista")
+    }
 }
-function eliminarTarea(nomTarea){
-    tarea.splice(tarea.indexOf(nomTarea),1,)
+function eliminarTarea(nomTarea) {
+    if (!tarea.includes(nomTarea)) {
+        alert("La tarea no existe en la lista")
+    } else {
+        tarea.splice(tarea.indexOf(nomTarea), 1,)
+    }
 }
 
 let cont = 1
 let num
 let tarea = []
-let list = ""
-
 while (cont !== 0) {
     num = parseInt(prompt(`Administrador de tareas: 
 1.Agregar Tarea
@@ -33,17 +53,17 @@ while (cont !== 0) {
 5.Salir del administrador de tareas`))
     switch (num) {
         case 1:
-            agregarTarea(prompt("ingrese una tarea: "))
+            agregarTarea(prompt("Ingrese una tarea: "))
             break;
         case 2:
             listarTarea()
             break;
         case 3:
-            editarTarea(prompt("Ingrese el nombre de la tarea a eliminar: "))
+            editarTarea(prompt("Ingrese el nombre de la tarea a editar: "))
             break;
         case 4:
             eliminarTarea(prompt("Ingrese el nombre de la tarea ha eliminar: "))
-            break;1
+            break; 1
         case 5:
             cont = 0
             break;
